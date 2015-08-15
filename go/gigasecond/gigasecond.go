0169bc67f08c5e160f9755a134e36aaf33097cd8
Fabio Pozzi <pozzi.fabio@gmail.com>
2015-08-15 20:51:42 +0200

    refactoring to make the code more readable
    . avoid useless intermediate value

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -4,18 +4,14 @@ package gigasecond
 // Also define a variable Birthday set to your (or someone else's) birthday.
 // Run go test -v to see your gigasecond anniversary.
 
-import (
-	"time"
-)
+import "time"
 
 const TestVersion = 2
+
 const shortForm = "2006-01-02"
 
 var Birthday, _ = time.Parse(shortForm, "1984-09-10")
 
-func AddGigasecond(b time.Time) time.Time {
-	t := time.Duration(1000000000) * time.Second
-	t1 := b.Add(t)
-
-	return t1
+func AddGigasecond(t time.Time) time.Time {
+	return t.Add(1e9 * time.Second)
 }
